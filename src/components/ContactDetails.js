1f2652961587bb2173c4a35bb492efbc78a383d3
mdshayequeahmad <shayeque01@gmail.com>
2023-09-09 01:32:44 +0530

    Edit User Details implemented.

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -7,12 +7,15 @@ import { AiOutlineGlobal } from "react-icons/ai";
 import { useNavigate } from 'react-router-dom';
 
 const ContactDetails = () => {
-    const [fullname, setFullname] = useState("");
-    const [url, setUrl] = useState("");
+
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    const [fullname, setFullname] = useState(user !== null ? user.displayName : "");
+    const [url, setUrl] = useState(user !== null ? user.photoURL : "");
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const auth = getAuth();
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -41,7 +44,7 @@ const ContactDetails = () => {
             <div className='flex'>
                 <div className='w-1/4 my-3'>
                 </div>
-                <div className='flex justify-between w-3/4 my-3 mx-5'>
+                <div className='flex justify-between w-3/4 my-3 mr-5'>
                     <h1 className='font-semibold text-2xl'>Contact Details</h1>
                     <button
                         className='text-red-600 border-solid border-2 border-red-600 rounded-lg px-3 
@@ -54,7 +57,7 @@ const ContactDetails = () => {
             <div className='flex'>
                 <div className='w-1/4'>
                 </div>
-                <div className='w-3/4 my-3 mx-5'>
+                <div className='w-3/4 my-3'>
                     <form onSubmit={submitHandler}>
                         <div className='flex flex-row items-center space-x-2'>
                             <div className='flex flex-row items-center space-x-2 mr-40'>
@@ -86,6 +89,7 @@ const ContactDetails = () => {
                             Update
                         </button>
                     </form>
+                    <hr className=' border-black my-5' />
                 </div>
             </div>
         </div>
