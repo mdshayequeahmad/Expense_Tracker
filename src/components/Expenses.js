7d220ee72c9c1f4891e0234939a0cc4ace585ea0
mdshayequeahmad <shayeque01@gmail.com>
2023-09-16 14:53:33 +0530

    bug fixed

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -7,18 +7,18 @@ const Expenses = () => {
     return (
         <div className='mt-10'>
             <h1 className='font-semibold text-3xl text-center mb-5'>Expenses</h1>
-            <div className='mx-40 px-4 bg-slate-200 rounded-md'>
-                {expenseItem.map((item, i) => (
-                    <div>
+            {expenseItem.map((item, i) => (
+                <div>
+                    <div className='mx-40 px-4 bg-slate-200 rounded-md'>
                         <p key={i} className='flex justify-between items-center text-lg h-12 font-semibold'>
                             <span>Rs: {item.amount}</span>
                             <span className=''>{item.description}</span>
                             <span>{item.category}</span>
                         </p>
-                        <hr className='bg-white h-1' />
                     </div>
-                ))}
-            </div>
+                    <div className='h-1' />
+                </div>
+            ))}
         </div>
     )
 }
